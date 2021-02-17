54e78ea6aa7676f7a91359a47981903c71ff3664
kaitoohsiro <kaitooshiro7@gmail.com>
2021-02-18 01:48:41 +0900

    first commit

---
+++ pages/write/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <amplify-authenticator>
+    <CreateBlog />
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
+</template>
+
+<script>
+import CreateBlog from '../components/writeBlog/create'
+
+export default {
+  name: 'Create',
+  components: {
+    CreateBlog,
+  },
+}
+</script>
